028476db8ca5e3e5f5787b0e9b9c28df9c256846
“Hodei” <hodei.martinezag@elorrieta-errekamari.com>
2023-11-27 12:25:32 +0100

    Crear clases y empezar a poner imagenes

---
+++ app/src/main/java/com/example/didaktikapp/Activity/Gune_4_Activity.java
@@ -0,0 +1,16 @@
+package com.example.didaktikapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.didaktikapp.R;
+
+public class Gune_4_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gune_4);
+    }
+}
--- app/src/main/java/com/example/didaktikapp/Fragments/Audio_Fragment_Gune_3.java
+++ app/src/main/java/com/example/didaktikapp/Fragments/Audio_Fragment_Gune_3.java
@@ -1,3 +1,4 @@
+
 package com.example.didaktikapp.Fragments;
 
 import android.content.SharedPreferences;
@@ -20,10 +21,10 @@ import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Audio_Fragment_Gune_1#newInstance} factory method to
+ * Use the {@link Audio_Fragment_Gune_3#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Audio_Fragment_Gune_1 extends Fragment {
+public class Audio_Fragment_Gune_3 extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +35,7 @@ public class Audio_Fragment_Gune_1 extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Audio_Fragment_Gune_1() {
+    public Audio_Fragment_Gune_3() {
         // Required empty public constructor
     }
 
@@ -44,36 +45,26 @@ public class Audio_Fragment_Gune_1 extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Frag2.
+     * @return A new instance of fragment Audio_Fragment_Gune_2.
      */
     // TODO: Rename and change types and number of parameters
-    public static Audio_Fragment_Gune_1 newInstance(String param1, String param2) {
-        Audio_Fragment_Gune_1 fragment = new Audio_Fragment_Gune_1();
+    public static Audio_Fragment_Gune_3 newInstance(String param1, String param2) {
+        Audio_Fragment_Gune_3 fragment = new Audio_Fragment_Gune_3();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1,param1);
+        args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-
         }
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_audio_gune_1, container, false);
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio_gune_1, container, false);
-    }
-
-
     private Button btn_hasi;
     private Button btn_pausatu;
     private MediaPlayer player;
@@ -86,10 +77,10 @@ public class Audio_Fragment_Gune_1 extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
-        btn_hasi = view.findViewById(R.id.btn_HasieraA);
-        btn_pausatu = view.findViewById(R.id.btn_PausaA);
-        barraAudio = view.findViewById(R.id.id_audioBarra_G1);
-        player = MediaPlayer.create(getActivity(), R.raw.audio_kontserbak);
+        btn_hasi = view.findViewById(R.id.btn_hasiera_G2);
+        btn_pausatu = view.findViewById(R.id.btn_pausatu_G2);
+        barraAudio = view.findViewById(R.id.id_audioBarra_G2);
+        player = MediaPlayer.create(getActivity(), R.raw.saregileak);
         barraAudio.setMax(player.getDuration());
         prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         runnable = new Runnable() {
